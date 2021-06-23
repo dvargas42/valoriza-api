15e3d0ff1cfbf34f53c3918f38b6997fe046bf6e
dvargas42 <dvargas.eng@gmail.com>
2021-06-22 22:12:20 -0300

    feat: create async erros middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata"
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
 
-import express from 'express'
 import { router } from './routes'
 
 import "./database"
@@ -10,6 +11,19 @@ const app = express()
 app.use(express.json())
 
 app.use(router)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: "Internal Server Error"
+  })
+})
 
 app.listen(3333, () => {
   console.log('🚀 Server is running!')
