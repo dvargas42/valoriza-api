e6077e560dbadb85c5c9b5e74b4341bd6139fd7f
dvargas42 <dvargas.eng@gmail.com>
2021-06-24 00:09:18 -0300

    feat: create sessions routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,17 @@ import { Router } from 'express'
 
 import { CreateUserController } from './controllers/CreateUserController'
 import { CreateTagController } from './controllers/CreateTagController'
+import { CreateSessionController } from './controllers/CreateSessionController'
 import { ensureAdmin } from './middlewares/ensureAdmin'
 
 const router = Router()
 
 const createUserController = new CreateUserController
 const createTagController = new CreateTagController
+const createSessionController = new CreateSessionController
 
 router.post('/users', createUserController.handle)
 router.post('/tags', ensureAdmin, createTagController.handle)
+router.post('/sessions', createSessionController.handle)
 
 export { router }
