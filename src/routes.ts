b1c26a3920e5914a2b7af455418e3639709a6bcb
dvargas42 <dvargas.eng@gmail.com>
2021-06-24 00:10:28 -0300

    feat: create compliments router

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import { CreateUserController } from './controllers/CreateUserController'
 import { CreateTagController } from './controllers/CreateTagController'
 import { CreateSessionController } from './controllers/CreateSessionController'
+import { CreateComplimentController } from './controllers/CreateComplimentController'
 import { ensureAdmin } from './middlewares/ensureAdmin'
 
 const router = Router()
@@ -10,9 +11,11 @@ const router = Router()
 const createUserController = new CreateUserController
 const createTagController = new CreateTagController
 const createSessionController = new CreateSessionController
+const createComplimentController = new CreateComplimentController
 
 router.post('/users', createUserController.handle)
 router.post('/tags', ensureAdmin, createTagController.handle)
 router.post('/sessions', createSessionController.handle)
+router.post('/compliments', createComplimentController.handle)
 
 export { router }
