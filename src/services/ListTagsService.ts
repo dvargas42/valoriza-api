dbe91940be7795c176f22eba950e5189f574c848
dvargas42 <dvargas.eng@gmail.com>
2021-06-25 21:53:13 -0300

    :tada: initial commit

---
+++ src/services/ListTagsService.ts
@@ -0,0 +1,17 @@
+import { getCustomRepository } from "typeorm"
+import { classToPlain } from "class-transformer"
+
+import { TagsRepositories } from "../repositories/TagsRepositories"
+
+class ListTagsService {
+  async execute() {
+    const tagsRepositories = getCustomRepository(TagsRepositories)
+
+    const tags = await tagsRepositories.find()
+    //tags = tags.map((tag) => ({ ...tag, nameCustom: `#${tag.name}` }))
+
+    return classToPlain(tags)
+  }
+}
+
+export { ListTagsService }
